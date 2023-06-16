2f5d8b5783b94fab89383fa53a86a23a0b22a75d
Daniel Rendox <daniel.rendox@gmail.com>
2023-06-16 12:55:36 +0300

    Solved synchroniation problem
    Main thread didn't wait for other threads to finish

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Main {
     public static void main(String[] args) {
         int consumersNumber = 10;
         DonutStorage donutStorage = new DonutStorage(20);
         ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        List<Future<?>> futures = new ArrayList<>(consumersNumber);
         for (int i = 0; i < consumersNumber; i++) {
-            executor.submit(() -> new Consumer(donutStorage).consume(1));
+            futures.add(executor.submit(() -> new Consumer(donutStorage).consume(1)));
         }
         executor.shutdown();
+
+        // make the main thread wait for others to finish
+        for (Future<?> future: futures) {
+            try {
+                future.get();
+            } catch (InterruptedException | ExecutionException e) {
+                System.out.println("Exception while getting from future" + e.getMessage());
+                e.printStackTrace();
+            }
+        }
 
         System.out.println("Number of remaining donuts: " + donutStorage.getDonutsNumber());
     }
